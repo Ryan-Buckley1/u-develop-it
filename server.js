ef729dce97395c0f46ade3ccb8745f6d0e27def6
Buckimus <ryan42buck@gmail.com>
2022-08-01 20:21:42 -0400

    added db to server

--- server.js
+++ server.js
@@ -1,14 +1,29 @@
 const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require("mysql2");
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database: "election",
+  },
+  console.log("connected to the election database")
+);
 
 app.get("/", (req, res) => {
   res.json({
     message: "Hello World",
   });
+});
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+  console.log(rows);
 });
 
 app.use((req, res) => {
